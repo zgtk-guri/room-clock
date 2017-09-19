eab553c0e578b235eabbc92fc85ef14cf50bc038
zgtk-guri <11141699+zgtk-guri@users.noreply.github.com>
2017-09-20 00:55:07 +0900

    Add Processing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,21 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import processing.core.PApplet
+
+class ProcessingMain : PApplet(){
+    override fun settings() {
+        size(1440, 900)
+    }
+
+    override fun setup() {
+        background(0)
+    }
+
+    override fun draw() {
+        fill(255)
+        noStroke()
+        ellipse(100.0f,100.0f,100.0f,100.0f)
+    }
+
+    fun run(args: Array<String>) : Unit = PApplet.main("ProcessingMain")
 }
+
+fun main(args: Array<String>) : Unit = ProcessingMain().run(args)
